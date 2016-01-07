2bf174c015ba689f38d07b753ef5b194c4fa545c
fredrickson <fredrickson@users.noreply.github.com>
2016-01-07 15:47:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,27 @@
 
 // Example 1-1: stroke and fill
 var x;
+var y;
 var count;
 function setup() {
   createCanvas(480, 270);
   stroke(0); 
   fill(150);
   x=0;
+  y=50;
   count=0;
 }
 
 function draw() {
   background(255);
   text(count,240,40);
-  rect(x,50,50,100);
+  rect(x,y,50,100);
   x=x+1;
   if (touchIsDown){
-    if (touchX>x && touchX<x+50 && touchY>50 && touchY<150){
+    if (touchX>x && touchX<x+50 && touchY>y && touchY<y+100){
       x=-50;
       count+=1;
+      y=random(50,500);
     }
   }
   
