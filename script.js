846a6374d9f39c0eee85bffc933296e889a5d183
fredrickson <fredrickson@users.noreply.github.com>
2016-01-07 15:09:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Learning Processing
+// Daniel Shiffman
+// http://www.learningprocessing.com
+
+// Example 1-1: stroke and fill
+var x;
+
+function setup() {
+  createCanvas(480, 270);
+  stroke(0); 
+  fill(150);
+  x=0;
+}
+
+function draw() {
+  background(255);
+  rect(x,50,75,100);
+  x+=1;
+}
